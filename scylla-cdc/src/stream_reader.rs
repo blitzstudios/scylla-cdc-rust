9da8584c7cb78bf1854dc6f407fc38747be90331
Victor Tsetsulin <victor@sleeper.app>
2025-10-02 15:26:13 -0400

    set page size to 5k

--- scylla-cdc/src/stream_reader.rs
+++ scylla-cdc/src/stream_reader.rs
@@ -120,7 +120,8 @@ impl StreamReader {
         );
         let mut query_cache = self.query_cache.lock().await;
         if !query_cache.contains_key(&query) {
-            let stmt = self.session.prepare_statement(query.clone()).await?;
+            let mut stmt = self.session.prepare_statement(query.clone()).await?;
+            stmt.set_page_size(5000);
             query_cache.insert(query.clone(), stmt);
         }
         let query_base = query_cache.get(&query).unwrap();
